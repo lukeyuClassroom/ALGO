adebd134e3a64f0e46a659f8dc357b0178b670c8
wangwuming <wangwuming@crland.com.cn>
2023-05-06 15:30:06 +0800

    init

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App.vue'
 
+import 'vuesax/dist/vuesax.css'
 Vue.config.productionTip = false
-
+Vue.use(Vuesax)
 new Vue({
   render: function (h) { return h(App) },
 }).$mount('#app')
